4a6a25b09cb753d6acb1ef96190ccb13c55e29d8
op30mmd <152014555+op30mmd@users.noreply.github.com>
2023-11-26 17:58:48 +0330

    Update test_nicehm.py

--- test_nicehm.py
+++ test_nicehm.py
@@ -1,9 +1,8 @@
-# test_capitalize.py
-
-def capitalize_string(s):
-    if not isinstance(s, str):
-        raise TypeError('Please provide a string')
-    return s.capitalize()
-
-def test_capitalize_string():
-    assert capitalize_string('test') == 'Test'
+import sys
+import pytest
+import traceback
+try:
+    pytest.main(sys.argv[1:])
+except BaseException:
+    traceback.print_exc()
+    raise
