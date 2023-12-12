60d8dd9e8630daa3b51e4cf14fc9260aedde93c2
op30mmd <152014555+op30mmd@users.noreply.github.com>
2023-12-12 16:06:16 +0330

    update main.py

--- main.py
+++ main.py
@@ -4,100 +4,111 @@ import shutil
 
 def main():
   """
-  Main function of the CLI file manager.
+  Main function of the interactive CLI file manager.
   """
-  parser = argparse.ArgumentParser(description="A simple CLI file manager")
+  # Initialize variables
+  current_directory = os.getcwd()
+  command = None
 
-  # Create a mutually exclusive group for conflicting options
-  exclusive_group = parser.add_mutually_exclusive_group()
+  # Welcome message
+  print("Welcome to the interactive CLI file manager!")
 
-  # Add options to the group
-  exclusive_group.add_argument("-help", action="store_true", help="Display help message")
-  exclusive_group.add_argument("-l", "--list", action="store_true", help="List files and directories")
-  parser.add_argument("-d", "--directory", default=os.getcwd(), help="Set the starting directory")
-  parser.add_argument("-c", "--copy", help="Copy a file or directory")
-  parser.add_argument("-m", "--move", help="Move a file or directory")
-  parser.add_argument("-r", "--rename", nargs=2, help="Rename a file or directory")
-  parser.add_argument("-p", "--permission", help="Change file permissions")
+  while command != "exit":
+    # Display current directory
+    print(f"\nCurrent directory: {current_directory}")
 
-  args = parser.parse_args()
+    # Prompt user for command
+    command = input("Enter command: ").strip()
 
-  # Set starting directory
-  os.chdir(args.directory)
+    # Handle different commands
+    if command == "ls":
+      list_files_and_directories(current_directory)
+    elif command.startswith("cd"):
+      current_directory = change_directory(current_directory, command)
+    elif command.startswith("mkdir"):
+      create_directory(current_directory, command)
+    elif command.startswith("rm"):
+      remove_file_or_directory(current_directory, command)
+    elif command.startswith("mv"):
+      move_file_or_directory(current_directory, command)
+    elif command.startswith("cp"):
+      copy_file_or_directory(current_directory, command)
+    elif command.startswith("rename"):
+      rename_file_or_directory(current_directory, command)
+    elif command.startswith("chmod"):
+      change_file_permissions(current_directory, command)
+    elif command == "help":
+      print_help()
+    elif command == "exit":
+      print("Bye!")
+    else:
+      print(f"Unknown command: '{command}'")
 
-  # Handle different commands based on parsed arguments
-  if args.help:
-    print_help()
-  elif args.list:
-    list_files_and_directories()
-  elif args.copy:
-    copy_file_or_directory(args.copy)
-  elif args.move:
-    move_file_or_directory(args.move)
-  elif args.rename:
-    rename_file_or_directory(args.rename[0], args.rename[1])
-  elif args.permission:
-    change_file_permissions(args.permission)
-  else:
-    print("No command specified. Use -h or --help for available commands.")
-
-def list_files_and_directories():
+def list_files_and_directories(current_directory):
   """
-  List files and directories in the current directory.
+  List files and directories in the specified directory.
   """
-  for entry in os.listdir():
-    if os.path.isfile(entry):
+  for entry in os.listdir(current_directory):
+    if os.path.isfile(os.path.join(current_directory, entry)):
       print(f"\t{entry}")
-    elif os.path.isdir(entry):
+    elif os.path.isdir(os.path.join(current_directory, entry)):
       print(f"\t[DIR] {entry}")
 
-def copy_file_or_directory(source):
+def change_directory(current_directory, command):
   """
-  Copy a file or directory.
+  Change the current directory.
   """
-  target = input("Enter target path: ")
   try:
-    if os.path.isdir(source):
-      shutil.copytree(source, target)
-    else:
-      shutil.copy2(source, target)
-    print(f"Successfully copied '{source}' to '{target}'")
-  except Exception as e:
-    print(f"Error copying '{source}': {e}")
+    new_directory = os.path.join(current_directory, command[3:])
+    os.chdir(new_directory)
+    return new_directory
+  except FileNotFoundError:
+    print(f"Error: Directory '{command[3:]}' does not exist.")
+    return current_directory
 
-def move_file_or_directory(source):
+def create_directory(current_directory, command):
+  """
+  Create a new directory.
+  """
+  try:
+    new_directory = os.path.join(current_directory, command[4:])
+    os.mkdir(new_directory)
+  except FileExistsError:
+    print(f"Error: Directory '{command[4:]}' already exists.")
+
+def remove_file_or_directory(current_directory, command):
+  """
+  Remove a file or directory.
+  """
+  try:
+    target_path = os.path.join(current_directory, command[3:])
+    if os.path.isfile(target_path):
+      os.remove(target_path)
+    elif os.path.isdir(target_path):
+      shutil.rmtree(target_path)
+  except FileNotFoundError:
+    print(f"Error: File or directory '{command[3:]}' does not exist.")
+
+def move_file_or_directory(current_directory, command):
   """
   Move a file or directory.
   """
-  target = input("Enter target path: ")
   try:
-    shutil.move(source, target)
-    print(f"Successfully moved '{source}' to '{target}'")
+    source_path = os.path.join(current_directory, command[3:])
+    target_path = input("Enter target path: ")
+    shutil.move(source_path, target_path)
   except Exception as e:
-    print(f"Error moving '{source}': {e}")
+    print(f"Error moving '{command[3:]}': {e}")
 
-def rename_file_or_directory(source, target):
+def copy_file_or_directory(current_directory, command):
   """
-  Rename a file or directory.
+  Copy a file or directory.
   """
   try:
-    os.rename(source, target)
-    print(f"Successfully renamed '{source}' to '{target}'")
-  except Exception as e:
-    print(f"Error renaming '{source}': {e}")
-
-def change_file_permissions(command):
-  """
-  Change file permissions.
-  """
-  # Implement permission change functionality
-  print("Feature under development!")
-
-def print_help():
-  """
-  Display a help message.
-  """
-  print(parser.format_help())
-
-if __name__ == "__main__":
-  main()
+    source_path = os.path.join(current_directory, command[3:])
+    target_path = input("Enter target path: ")
+    if os.path.isdir(source_path):
+      shutil.copytree(source_path, target_path)
+    else:
+      shutil.copy2(source_path, target_path)
+  except Exception as e
