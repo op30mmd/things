fbe7e8ee48efc420b631e194641b03fe2f23a038
op30mmd <152014555+op30mmd@users.noreply.github.com>
2023-12-12 07:16:08 +0330

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-def my_function(x):
-  logger.info("Starting my_function with x={}".format(x))
+from flask import Flask, request, send_file
+import requests
 
-  # Define the result variable before using it
-  result = x * 2
+app = Flask(__name__)
 
-  logger.debug("Returning {}".format(result))
-  return result
+@app.route("/download", methods=["POST"])
+def download_file():
+  url = request.form["url"]
+
+  try:
+    # Make a request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+      # Get the filename from the response headers
+      filename = response.headers.get("Content-Disposition").split("filename=")[1]
+
+      # Write the content to a file
+      with open(filename, "wb") as f:
+        f.write(response.content)
+
+      # Send the file to the user
+      return send_file(filename, as_attachment=True)
+    else:
+      return "Error: Could not download file.", 400
+  except Exception as e:
+    return "Error: {}".format(e), 500
+
+if __name__ == "__main__":
+  app.run(debug=True)
