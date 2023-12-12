8779e91534e54ae28d8a2d74cf9f2ba77dd3704f
op30mmd <152014555+op30mmd@users.noreply.github.com>
2023-12-12 15:43:25 +0330

    update main.py

--- main.py
+++ main.py
@@ -1,36 +1,86 @@
-from fastapi import FastAPI, Request, File, UploadFile
-import requests
+import os
 
-app = FastAPI()
-
-@app.post("/download")
-async def download_file(url: str, file: UploadFile = None):
+def main():
   """
-  Download a file from a URL.
-
-  Args:
-      url: The URL of the file to download.
-      file: (optional) An uploaded file containing the download URL.
-
-  Returns:
-      A JSON response with the downloaded file content and filename.
+  Main function of the CLI file manager.
   """
+  # Display current directory
+  print(f"Current directory: {os.getcwd()}")
 
-  try:
-    if file:
-      # Get the URL from the uploaded file content
-      url = await file.read()
-    # Make a request to the URL
-    response = requests.get(url)
+  # Get user input
+  user_input = ""
+  while user_input.lower() != "exit":
+    user_input = input("> ").strip()
 
-    # Check if the request was successful
-    if response.status_code == 200:
-      # Generate a filename based on the URL
-      filename = url.split("/")[-1]
-
-      # Return the downloaded file content and filename
-      return {"filename": filename, "content": response.content}
+    # Handle different commands
+    if user_input == "ls":
+      # List files and directories
+      list_files_and_directories()
+    elif user_input.startswith("cd"):
+      # Change directory
+      change_directory(user_input)
+    elif user_input.startswith("mkdir"):
+      # Create directory
+      create_directory(user_input)
+    elif user_input.startswith("rm"):
+      # Remove file or directory
+      remove_file_or_directory(user_input)
+    elif user_input == "help":
+      # Display help message
+      print_help()
     else:
-      return {"error": "Error: Could not download file."}, 400
-  except Exception as e:
-    return {"error": "Error: {}".format(e)}, 500
+      print(f"Unknown command: '{user_input}'")
+
+def list_files_and_directories():
+  """
+  List files and directories in the current directory.
+  """
+  for entry in os.listdir():
+    if os.path.isfile(entry):
+      print(f"\t{entry}")
+    elif os.path.isdir(entry):
+      print(f"\t[DIR] {entry}")
+
+def change_directory(command):
+  """
+  Change the current directory.
+  """
+  try:
+    os.chdir(command[3:])
+  except FileNotFoundError:
+    print(f"Error: Directory '{command[3:]}' does not exist.")
+
+def create_directory(command):
+  """
+  Create a new directory.
+  """
+  try:
+    os.mkdir(command[4:])
+  except FileExistsError:
+    print(f"Error: Directory '{command[4:]}' already exists.")
+
+def remove_file_or_directory(command):
+  """
+  Remove a file or directory.
+  """
+  try:
+    os.remove(command[3:])
+  except FileNotFoundError:
+    print(f"Error: File or directory '{command[3:]}' does not exist.")
+  except IsADirectoryError:
+    os.rmdir(command[3:])
+
+def print_help():
+  """
+  Display a help message.
+  """
+  print("\nAvailable commands:")
+  print("\tls: List files and directories in the current directory.")
+  print("\tcd <directory>: Change the current directory.")
+  print("\tmkdir <directory>: Create a new directory.")
+  print("\trm <file/directory>: Remove a file or directory.")
+  print("\thelp: Display this help message.")
+  print("\texit: Quit the program.\n")
+
+if __name__ == "__main__":
+  main()
